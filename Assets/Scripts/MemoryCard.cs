88de606001a41c41fd274a0f2fc1aeb59fcd5c18
Gustavo Reyes <gustavo.reyes1@upr.edu>
2017-08-23 11:38:48 -0500

    initialized repository, game halfway done

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject cardBack;
+    [SerializeField]
+    private SceneControllers controller;
+
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void OnMouseDown()
+    {
+        cardBack.SetActive(false);
+    }
+}
